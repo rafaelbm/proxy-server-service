3f19429f6f8907bf1bf4eb3acfc164fc39ef3c1d
rafaelbm <5865557+rafaelbm@users.noreply.github.com>
2016-10-16 23:40:06 -0200

    Initial commit

---
+++ ProxyServerService/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace ProxyServerService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HostFactory.Run(serviceConfig => 
+            {
+                serviceConfig.Service<ProxyService>(serviceInstance =>
+                {
+                    serviceInstance
+                        .ConstructUsing(() => new ProxyService())
+                        .WhenStarted(execute => execute.Start())
+                        .WhenStopped(execute => execute.Stop());
+                });
+                serviceConfig.SetServiceName("SampleProxyServer");
+                serviceConfig.SetDisplayName("Sample Proxy Server");
+
+                serviceConfig.StartManually();
+            });
+        }
+    }
+}
